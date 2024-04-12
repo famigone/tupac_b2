5ed47994911469aa5eb7ceda4f2c7a3260d93fd5
Fede <fede.amigone@gmail.com>
2024-04-12 09:52:25 -0300

    asdf

--- frontend/generated/vaadin.ts
+++ frontend/generated/vaadin.ts
@@ -25,3 +25,7 @@ import 'Frontend/generated/jar-resources/vaadin-dev-tools/vaadin-dev-tools.js';
 import './theme-tupak.global.generated.js';
 import { applyTheme } from './theme.js';
 applyTheme(document);
+//@ts-ignore
+if (import.meta.env.DEV) {
+    import("Frontend/generated/jar-resources/dev-tools-database.js");
+}
